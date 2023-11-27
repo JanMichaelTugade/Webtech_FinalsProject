b72855f4bb47d35626064c474384cda3df492691
GREG BANGYAY <2222764>
2023-11-27 17:10:12 +0800

    bugfix

--- cms/logout.php
+++ cms/logout.php
@@ -1,16 +1,16 @@
 <?php 
 require_once 'dbcon.php';
+session_start();
+
+$updateQuery = "UPDATE user SET status = 'Offline' WHERE username = ?";
+$updateStmt = $conn->prepare($updateQuery);
+$updateStmt->bind_param("s", $_SESSION['username']);
+$updateStmt->execute();
+$updateStmt->close();
 
 session_start();
 session_unset();
 session_destroy();
-
-$updateQuery = "UPDATE user SET status = 'Offline' WHERE username = ?";
-$updateStmt = $conn->prepare($updateQuery);
-$updateStmt->bind_param("s", $username);
-$updateStmt->execute();
-$updateStmt->close();
-
 
 header('Location: index.php');
 
