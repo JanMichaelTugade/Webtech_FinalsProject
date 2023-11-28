4c6866d6acbe91cd08c9d835079ab61c93b5fc58
JESSE <2221096@slu.edu.ph>
2023-11-28 08:54:27 +0800

    functional add user

--- admin/routes/user-management.js
+++ admin/routes/user-management.js
@@ -23,4 +23,45 @@ router.get("/", function(request, response, next){
 
 });
 
+router.get("/add", function(request, response, next){
+
+	response.render("um", {title:'Add User', action:'add'});
+
+});
+
+router.post("/add_user_data", function(request, response, next){
+
+	var username = request.body.username;
+
+	var password = request.body.password;
+
+    var fname = request.body.fname;
+
+    var lname = request.body.lname;
+
+	var role = request.body.role;
+
+	var status = 'offline';
+
+	var query = `
+        INSERT INTO user 
+        (username, password, fname, lname, role, status) 
+        VALUES ("${username}", "${password}", "${fname}", "${lname}", "${role}", "${status}")
+    `;
+
+	database.query(query, function(error, data){
+
+		if(error)
+		{
+			throw error;
+		}	
+		else
+		{
+			response.redirect("/user-management");
+		}
+
+	});
+
+});
+
 module.exports = router;
