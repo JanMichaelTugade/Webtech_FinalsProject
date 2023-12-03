fde2e218bd3105b29b8c7037b301ae4597f118bc
JESSE <2221096@slu.edu.ph>
2023-12-03 11:35:49 +0800

    login admin pero wala pang session handling

--- admin/routes/user-management.js
+++ admin/routes/user-management.js
@@ -4,6 +4,33 @@ var router = express.Router();
 
 var database = require('../database');
 
+// // Login route
+// router.get("/", function(request, response, next){
+//     response.render("login", { title: 'Login' }); // Render your login page
+// });
+
+// router.post("/", function(request, response, next){
+//     var username = request.body.username;
+//     var password = request.body.password;
+
+//     var query = `SELECT * FROM user WHERE username = "${username}" AND password = "${password}"`;
+
+//     database.query(query, function(error, data){
+//         if(error) {
+//             throw error;
+//         } else {
+//             if(data.length > 0) {
+//                 // User exists, redirect to user management or dashboard
+//                 response.redirect("/login/user-management");
+//             } else {
+//                 // User not found or incorrect password, handle accordingly
+//                 response.render("login", { title: 'Login', error: 'Invalid username or password' });
+//             }
+//         }
+//     });
+// });
+
+
 router.get("/", function(request, response, next){
 
 	var query = "SELECT * FROM user ORDER BY username DESC";
@@ -16,7 +43,7 @@ router.get("/", function(request, response, next){
 		}
 		else
 		{
-			response.render('um', {title:'User Management', action:'list', sampleData:data});
+			response.render('um', {title:'User Management', action:'list', sampleData:data, session : request.session});
 		}
 
 	});
@@ -25,46 +52,53 @@ router.get("/", function(request, response, next){
 
 router.get("/add", function(request, response, next){
 
-	response.render("um", {title:'Add User', action:'add'});
+	response.render('um', {title:'Add User', action:'add', error: null});
 
 });
-
 
 
 router.post("/add_user_data", function(request, response, next){
-
-	var username = request.body.username;
-
-	var password = request.body.password;
-
+    var username = request.body.username;
+    var password = request.body.password;
     var fname = request.body.fname;
-
     var lname = request.body.lname;
+    var role = request.body.role;
+    var status = 'offline';
 
-	var role = request.body.role;
+    // Check if username or name already exists
+    var checkQuery = `SELECT * FROM user WHERE username = "${username}" OR (fname = "${fname}" AND lname = "${lname}")`;
 
-	var status = 'offline';
+    database.query(checkQuery, function(checkError, checkData){
+        if(checkError) {
+            throw checkError;
+        } else {
+            if(checkData.length > 0) {
+                // User with similar username or name already exists
+                response.render('um', {
+                    title: 'Add User',
+                    action: 'add',
+                    error: 'Username or Name already exists'
+                });
+            } else {
+                // Insert the user into the database
+                var insertQuery = `
+                    INSERT INTO user 
+                    (username, password, fname, lname, role, status) 
+                    VALUES ("${username}", "${password}", "${fname}", "${lname}", "${role}", "${status}")
+                `;
 
-	var query = `
-        INSERT INTO user 
-        (username, password, fname, lname, role, status) 
-        VALUES ("${username}", "${password}", "${fname}", "${lname}", "${role}", "${status}")
-    `;
-
-	database.query(query, function(error, data){
-
-		if(error)
-		{
-			throw error;
-		}	
-		else
-		{
-			response.redirect("/user-management");
-		}
-
-	});
-
+                database.query(insertQuery, function(insertError, insertData){
+                    if(insertError) {
+                        throw insertError;
+                    } else {
+                        response.redirect('/user-management');
+                    }
+                });
+            }
+        }
+    });
 });
+
 
 // EDIT AND DELETE
 router.get('/edit/:username', function(request, response, next){
@@ -148,8 +182,6 @@ router.get('/delete/:username', function (request, response, next){
 });
 });
 
-module.exports = router;
-
 //SEARCH
 router.get("/search", function(request, response, next){
     var searchTerm = request.query.term;
@@ -173,3 +205,5 @@ router.get("/search", function(request, response, next){
         }
     });
 });
+
+module.exports = router;
