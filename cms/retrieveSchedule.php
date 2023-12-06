e0e2063380c701e44702d0da3336f2638946708e
Kiefer John M. Sanchez <2212077.slu.edu.ph>
2023-12-06 10:11:35 +0800

    gods gods gods

--- cms/retrieveSchedule.php
+++ cms/retrieveSchedule.php
@@ -2,12 +2,29 @@
 
 require_once 'dbcon.php';
 
-$query = "SELECT title, startTime, endTime FROM livestream";
+$query = "
+    SELECT combined_name,
+           livestream_startTime,
+           livestream_endTime
+    FROM (
+        SELECT livestream.title AS combined_name,
+               livestream.startTime AS livestream_startTime,
+               livestream.endTime AS livestream_endTime
+        FROM livestream
+
+        UNION ALL
+
+        SELECT content.name AS combined_name,
+               content.startTime AS content_startTime,
+               content.endTime AS content_endTime
+        FROM content
+    ) AS combined_data
+    ORDER BY livestream_startTime";
+
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
     $data = [];
-
 
     while ($row = $result->fetch_assoc()) {
         $data[] = $row;
