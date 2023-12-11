01b31452cf625df10da68a9461c473e7609fd7d3
gregbangyay <147226619+gregbangyay@users.noreply.github.com>
2023-12-11 14:54:31 +0800

    added webSocket_server
    > To run, open cmd in the webSocket_server directory
    > install composer
    > enter "php webSocket_server.php"

---
+++ cms/php/webSocket_server.php
@@ -0,0 +1,57 @@
+<?php
+require __DIR__ . '/../../../vendor/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+class MyWebSocketServer implements MessageComponentInterface
+{
+    protected $clients;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+        echo "New client connected: {$conn->resourceId}\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        // Broadcast the received message to all connected clients
+        foreach ($this->clients as $client) {
+            $client->send($msg);
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+        echo "Client disconnected: {$conn->resourceId}\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "An error occurred for client {$conn->resourceId}: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new MyWebSocketServer()
+        )
+    ),
+    8080 
+);
+
+echo "WebSocket server started\n";
+
+$server->run();
