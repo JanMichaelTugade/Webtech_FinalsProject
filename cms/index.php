7ddc20e4f89c599a94cb2f879b821572ea167bd5
gregbangyay <147226619+gregbangyay@users.noreply.github.com>
2023-12-12 16:47:53 +0800

    Update index.php

--- cms/index.php
+++ cms/index.php
@@ -3,7 +3,7 @@
 session_start();
 
 // Check if the user is already logged in
-if (isset($_SESSION['username'])) {
+if (isset($_SESSION['username']) && isset($_SESSION['uniqid'])) {
     include('php/View/cms.php');
 } else {
     include('php/View/login_cms.php');
