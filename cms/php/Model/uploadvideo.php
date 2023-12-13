d56b37ba7f7676005c2f3c044f38b07d45f28b33
Jan Michael Tugade <2220212@slu.edu.ph>
2023-12-13 12:56:43 +0800

    omsem

--- cms/php/Model/uploadvideo.php
+++ cms/php/Model/uploadvideo.php
@@ -48,7 +48,7 @@ if (isset($_FILES['file']) && isset($_POST['submit'])) {
         echo "invalid_extension: Invalid file extension.";
     }
 } else {
-    echo "no_file: No file submitted.";77
+    echo "no_file: No file submitted.";
 }
 
 function getVideoDuration($file_path, $file_extension) {
